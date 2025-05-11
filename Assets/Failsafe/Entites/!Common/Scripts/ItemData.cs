0facc1d3387b58d0d2d9929f5b85659f2e457e69
GeroiGorodskoyZastroiki <denhardx@gmail.com>
2025-05-11 15:24:44 +0300

    original architecture

---
+++ Assets/Failsafe/Entites/!Common/Scripts/ItemData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public string Description;
+}
---
+++ Assets/Failsafe/Entites/!Common/Scripts/PropData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PropData", menuName = "ScriptableObjects/PropData")]
+public class PropData : ScriptableObject
+{
+    public string Name;
+}

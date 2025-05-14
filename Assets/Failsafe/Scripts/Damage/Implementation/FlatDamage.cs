6d2df5936c8d448cda5a87f6299357f5dc377ca4
Dmitriy Svetilov <svetilov.dmitriy@gmail.com>
2025-05-14 19:05:44 +0200

    simple damage implementation

---
+++ Assets/Failsafe/Scripts/Damage/Implementation/FlatDamage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Failsafe.Scripts.Damage.Implementation
+{
+    [Serializable]
+    public class FlatDamage : IDamage
+    {
+        [field: SerializeField]
+        public float DamageAmount { get; private set; }
+
+        public FlatDamage(float damageAmount)
+        {
+            DamageAmount = damageAmount;
+        }
+    }
+}
---
+++ Assets/Failsafe/Scripts/Damage/Providers/FlatDamageProvider.cs
@@ -0,0 +1,19 @@
+﻿using Failsafe.Scripts.Damage.Implementation;
+
+namespace Failsafe.Scripts.Damage.Providers
+{
+    public class FlatDamageProvider : IDamageProvider<FlatDamage>
+    {
+        private readonly IDamageable _damageable;
+
+        public FlatDamageProvider(IDamageable damageable)
+        {
+            _damageable = damageable;
+        }
+        
+        public void Provide(FlatDamage damage)
+        {
+            _damageable.TakeDamage(damage.DamageAmount);
+        }
+    }
+}

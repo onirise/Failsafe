f6b1bf07f7daf79bae09783629f5f07a850da369
TheDeadSky <deadsky.1136@gmail.com>
2025-05-28 22:22:45 +0300

    feat: Добавлена переноска физических предметов и возможность их бросить

---
+++ Assets/Failsafe/Player/PlayerMovements/Scripts/PlayerStates/ThrowState.cs
@@ -0,0 +1,20 @@
+﻿using Failsafe.Player.Interaction;
+
+namespace Failsafe.PlayerMovements.States
+{
+    public class ThrowState : BehaviorState
+    {
+        private readonly PhysicsInteraction _physicsInteraction;
+
+
+        public ThrowState(PhysicsInteraction physicsInteraction)
+        {
+            _physicsInteraction = physicsInteraction;
+        }
+        
+        public override void Enter()
+        {
+            _physicsInteraction.ThrowObject();
+        }
+    }
+}
---
+++ Assets/Failsafe/Scripts/Player/Interaction/PhysicsInteraction.cs
@@ -0,0 +1,106 @@
+using Failsafe.PlayerMovements;
+using System;
+using System.IO.Enumeration;
+using UnityEngine;
+
+namespace Failsafe.Player.Interaction
+{
+    public class PhysicsInteraction : MonoBehaviour
+    {
+        [SerializeField] private float _carryingDistance = 2.5f;
+        [SerializeField] private float _maxPickupDistance = 5f;
+        
+        [SerializeField] private GameObject _carryingObject;
+        [SerializeField] private Rigidbody _carryingBody;
+        [SerializeField] private Transform _playerCameraTransform;
+        
+        [SerializeField] private Vector3 _draggablePositionOffset;
+        [SerializeField] private float _dragSpeed = 10f;
+        
+        [SerializeField] private float _throwForce = 10f;
+
+        private Quaternion _relativeRotation;
+        
+        public bool IsDragging { get; private set; }
+
+        private void Awake()
+        {
+            if (!_playerCameraTransform)
+            {
+                Camera playerCamera = transform.root.GetComponentInChildren<Camera>();
+                
+                _playerCameraTransform = playerCamera.transform;
+            }
+        }
+        
+        private void FixedUpdate()
+        {
+            if (_carryingObject)
+            {
+                DragObject();
+            }
+        }
+
+        public void GrabOrDrop()
+        {
+            if (!_carryingObject)
+            {
+                GrabObject();
+            }
+            else
+            {
+                DropItem();
+            }
+        }
+        
+        private void DragObject()
+        {
+            Vector3 targetPosition = transform.position + _playerCameraTransform.forward * _carryingDistance;
+            Quaternion targetRotation = transform.rotation * _relativeRotation;
+            
+            _carryingBody.linearVelocity = (targetPosition - _carryingBody.position + _draggablePositionOffset) * _dragSpeed;
+            
+            _carryingBody.rotation = targetRotation;
+            
+            _carryingBody.angularVelocity = Vector3.zero;
+        }
+        
+        private void GrabObject()
+        {
+            Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, _maxPickupDistance);
+
+            if (!hitInfo.rigidbody)
+                return;
+            
+            _carryingBody = hitInfo.rigidbody;
+            _carryingBody.useGravity = false;
+            
+            _carryingObject = hitInfo.rigidbody.gameObject;
+            
+            _carryingObject.transform.parent = transform;
+            _relativeRotation = _carryingObject.transform.localRotation;
+            _carryingObject.transform.parent = null;
+            
+            IsDragging = true;
+        }
+
+        public void ThrowObject()
+        {
+            _carryingBody.useGravity = true;
+            
+            _carryingBody.AddForce(_playerCameraTransform.forward * _throwForce, ForceMode.Impulse);
+            
+            _carryingBody = null;
+            _carryingObject = null;
+            IsDragging = false;
+        }
+        
+        private void DropItem()
+        {
+            _carryingBody.useGravity = true;
+            _carryingBody = null;
+            _carryingObject = null;
+            IsDragging = false;
+        }
+    }
+}

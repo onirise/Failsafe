ab7654c15158ac066225f9fb610c51a942068243
Riplestripe <ilyasokolow@mail.ru>
2025-06-10 23:26:54 +0200

    Main

--- Assets/Failsafe/Player/PlayerMovements/Scripts/Input/InputHandler.cs
+++ Assets/Failsafe/Player/PlayerMovements/Scripts/Input/InputHandler.cs
@@ -24,7 +24,8 @@ public class InputHandler
     private const string _crouch = "Crouch";
     private const string _grabOrDrop = "GrabOrDrop";
     private const string _attack = "Attack";
-
+    private const string _zoom = "Zoom";
+    
     private InputAction _movementAction;
     private InputAction _rotationAction;
     private InputAction _jumpAction;
@@ -32,7 +33,7 @@ public class InputHandler
     private InputAction _crouchAction;
     private InputAction _grabOrDropAction;
     private InputAction _attackAction;
-
+    private InputAction _zoomAction;    
     public Vector2 MovementInput { get; private set; }
     public bool MoveForward => MovementInput.y > 0;
     public bool MoveBack => MovementInput.y < 0;
@@ -41,7 +42,7 @@ public class InputHandler
     public bool SprintTriggered { get; private set; }
     public InputTrigger CrouchTrigger { get; private set; } = new InputTrigger(); public bool GrabOrDropTriggered { get; private set; }
     public bool AttackTriggered {get; private set;}
-
+    public bool ZoomTriggered {get; private set;}
 
     /// <summary>
     /// Преобразовать MovementInput к нужному Transform
@@ -64,7 +65,7 @@ public class InputHandler
         _crouchAction = mapReference.FindAction(_crouch);
         _grabOrDropAction = mapReference.FindAction(_grabOrDrop);
         _attackAction = mapReference.FindAction(_attack);
-
+        _zoomAction = mapReference.FindAction(_zoom);
         SubscribeActionValuesToInputEvents();
     }
 
@@ -90,6 +91,9 @@ public class InputHandler
         
         _attackAction.performed += inputInfo => AttackTriggered = true;
         _attackAction.canceled += inputInfo => AttackTriggered = false;
+        
+        _zoomAction.performed += inputInfo => ZoomTriggered = true;
+        _zoomAction.canceled += inputInfo => ZoomTriggered = false;
 
     }
     

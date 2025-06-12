0e39bdd7d3f8c46a790364d3dd3b0e5cf2e4514b
TrofimovEI <trofimoved@mail.ru>
2025-06-12 19:38:49 +0500

    Удалил тестовый код

--- Assets/Failsafe/GameSceneServices/Scripts/GameSceneLifetimeScope.cs
+++ Assets/Failsafe/GameSceneServices/Scripts/GameSceneLifetimeScope.cs
@@ -1,43 +1,17 @@
-using Failsafe.Player;
-using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 namespace Failsafe.GameSceneServices
 {
     /// <summary>
-    /// Регистрация сервисов и компонентов игровой сцены, общих для всей сцены
+    /// Регистрация сервисов и компонентов игровой сцены, общих для объектов на сцене или не привязаных к конкретному объекту
+    /// <para/>Дочерний скоуп к <see cref="Failsafe.Scripts.DependencyInjection.RootLifetimeScope"/>
     /// </summary>
     public class GameSceneLifetimeScope : LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.Register<SceneDependency>(Lifetime.Scoped);
-            //TODO: зарегистрировать системы игровой сцены : SignalManager, SpownSystem ...
-        }
-    }
-    //TODO: Все что ниже используется для теста зависимостей, удалить
-    public class SceneDependency
-    {
-        // Подтягиваются зависимости из дочернего скоупа, 
-        // но лучше так не делать, потому что дочерний скоуп может создаться позже или удалиться раньше текущего
-        PlayerDependency _playerDependency;
-
-        public SceneDependency(PlayerDependency playerDependency)
-        {
-            _playerDependency = playerDependency;
-        }
-
-
-        public void Method()
-        {
-            Debug.Log("Execute SceneDependency Method");
-        }
-
-        public void PlayerMethod()
-        {
-            Debug.Log("Execute SceneDependency PlayerMethod");
-            _playerDependency.Method();
+            //TODO: зарегистрировать системы игровой сцены : SignalManager, SpawnSystem ...
         }
     }
 }
--- Assets/Failsafe/Player/Scripts/PlayerLifetimeScope.cs
+++ Assets/Failsafe/Player/Scripts/PlayerLifetimeScope.cs
@@ -1,52 +1,17 @@
-using Failsafe.GameSceneServices;
-using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 namespace Failsafe.Player
 {
     /// <summary>
-    /// Регистрация комонентов игрового персонажа
+    /// Регистрация компонентов игрового персонажа
+    /// <para/>Дочерний скоуп к <see cref="Failsafe.GameSceneServices.GameSceneLifetimeScope"/>
     /// </summary>
     public class PlayerLifetimeScope : LifetimeScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.Register<PlayerDependency>(Lifetime.Scoped);
-            builder.RegisterEntryPoint<TestStart>();
-            //TODO: зарегистрировать системы игрока : InputHandler, MovementController ...
+            //TODO: зарегистрировать компоненты и системы игрока : InputHandler, MovementController, Inventory ...
         }
-    }
-    //TODO: Все что ниже используется для теста зависимостей, удалить
-    public class PlayerDependency
-    {
-        public void Method()
-        {
-            Debug.Log("Execute PlayerDependency Method");
-        }
-    }
-
-    public class TestStart : IStartable
-    {
-        // Можно подтягивать зависимости родительского скоупа
-        private SceneDependency _dependency;
-        private PlayerDependency _playerDependency;
-
-        public TestStart(SceneDependency dependency, PlayerDependency playerDependency)
-        {
-            _dependency = dependency;
-            _playerDependency = playerDependency;
-
-        }
-
-
-        public void Start()
-        {
-            Debug.Log("TestStart started");
-            _dependency.Method();
-            _playerDependency.Method();
-            _dependency.PlayerMethod();
-        }
-
     }
 }

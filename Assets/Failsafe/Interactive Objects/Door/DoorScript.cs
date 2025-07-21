7a7e29a20c3a2373bca23d0c547175b3a99b8dca
kiko <kirill_costomarov@mail.ru>
2025-07-21 20:58:12 +0300

    переделал скрипт, добавил возможность менять состояния двери в старт

--- Assets/Failsafe/Interactive Objects/Door/DoorScript.cs
+++ Assets/Failsafe/Interactive Objects/Door/DoorScript.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-public class DoorScript: MonoBehaviour
+public class DoorScript : MonoBehaviour
 {
     private Animator _animator;
     private Color _defoltColorLight;//это пока UI не прекрутили к двери
     private string _enemyTag = "Enemy";
 
-    private bool _isPowered;
     private bool _enemyBlockDoor = false;
     private bool _doorWasOpen = false;
 
+    [SerializeField] private bool _isPowered;
     [SerializeField] private bool _isOpen;
     [SerializeField] private Light[] _panelLights;// для отображения места для интерактива с дверью
 
@@ -18,17 +18,19 @@ public class DoorScript: MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _defoltColorLight = _panelLights[0].color;
+
+        EnableLight(_isPowered);
+        _animator.SetBool("isOpen", _isOpen);
     }
     public void OnPowered()
     {
         Debug.Log("Door power on");
         _isPowered = true;
-        foreach (Light light in _panelLights)
-            light.enabled = true;
+        EnableLight(_isPowered);
         if (_enemyBlockDoor)
         {
-            _isOpen = !_isOpen;
-            _animator.SetBool("isOpen", _isOpen);
+            _isOpen = true;
+            _animator.SetBool("isOpen", true);
             Debug.Log("Active Door");
         }
     }
@@ -36,57 +38,55 @@ public class DoorScript: MonoBehaviour
     {
         Debug.Log("Door power off");
         _isPowered = false;
-        foreach (Light light in _panelLights)
-            light.enabled = false;
+        EnableLight(_isPowered);
     }
-    private void OpenCloseDoor()
+    private void OpenCloseDoor(bool open)
     {
         if (!_isPowered) return;
         if (_enemyBlockDoor) return;
-        _isOpen = !_isOpen;
-        _animator.SetBool("isOpen", _isOpen);
+        _isOpen = open;
+        _animator.SetBool("isOpen", open);
         Debug.Log("Active Door");
     }
+
     private void OnMouseDown() //для примера вызов открытия/закрытия дверей
     {
-        OpenCloseDoor();
+        OpenCloseDoor(!_isOpen);
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
-        if (other.gameObject.tag == _enemyTag)
-        {
-            if (!_isOpen)
-            {
-                Debug.Log(other.gameObject.tag);
-                OpenCloseDoor();
-            }
-            else
-            {
-                _doorWasOpen = true;
-            }
-            _enemyBlockDoor = true;
-            foreach (Light light in _panelLights)
-                light.color = Color.red;
-        }
+        if (!other.gameObject.CompareTag(_enemyTag)) return;
+
+
+        _doorWasOpen = _isOpen;
+        OpenCloseDoor(true);
+        _enemyBlockDoor = true;
+        ChangeColor(Color.red);
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == _enemyTag)
+        if (!other.gameObject.CompareTag(_enemyTag)) return;
+
+        _enemyBlockDoor = false;
+        ChangeColor(_defoltColorLight);
+
+        if (_doorWasOpen) { return; }
+
+        OpenCloseDoor(false);
+    }
+
+    private void ChangeColor(Color lightColor)
+    {
+        foreach (Light panelLight in _panelLights)
         {
-            _enemyBlockDoor = false;
-            foreach (Light light in _panelLights)
-                light.color = _defoltColorLight;
-            if (_isOpen)
-            {
-                if (_doorWasOpen)
-                {
-                    _doorWasOpen = false;
-                    return;
-                }
-                Debug.Log(other.gameObject.tag);
-                OpenCloseDoor();
-            }
+            panelLight.color = lightColor;
         }
+    }
+    private void EnableLight (bool doorIsPowered)
+    {
+        foreach (Light light in _panelLights)
+            light.enabled = doorIsPowered;
     }
 }

f2a99043170e2fd189105ee800aef4242f6f46d3
Smagnyy <caif1118@gmail.com>
2025-07-21 23:58:17 +0500

    final

--- Assets/Failsafe/UI/Menu/OnSelectEffect.cs
+++ Assets/Failsafe/UI/Menu/OnSelectEffect.cs
@@ -10,31 +10,20 @@ public class OnSelectEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     Color _originalColor;
     Material _originMaterial;
     Image _buttonBackground;
+    [SerializeField] bool _addArrow;
     [SerializeField] Material _targetMaterial;
     [SerializeField] Color _targetColor;
-    // это текст, у которого должна появляться стрелочка при наведении
     [SerializeField] TextMeshProUGUI _mainTextMeshProUGUI;
-
-    // это тексты в случае если у объекта несколько текстов (как у профилей) и им не нужна стрелочка при наведении
-    //  у профилей вообще только эти тексты и заполнены, mainTextMeshProUGUI пуст
     [SerializeField] List<TextMeshProUGUI> _optionalTextsGO;
-    // задумка в том, что объекту могут быть одновременно нужны как изменения текста с добавлением стрелочки
-    // так и как раз дополнительные тексты, которым стрелка не нужна
-    // наследование я делать не хотел.
+
 
     private void Start()
     {
         _buttonBackground = GetComponent<Image>();
-        if (_mainTextMeshProUGUI != null)
-        {
-            _originMaterial = _mainTextMeshProUGUI.fontSharedMaterial;
-            _mainOriginalText = _mainTextMeshProUGUI.text;
-        }
-        else
-        {
-            _originMaterial = _optionalTextsGO[0].fontSharedMaterial;
-        }
 
+        _originMaterial = _mainTextMeshProUGUI.fontSharedMaterial;
+        _mainOriginalText = _mainTextMeshProUGUI.text;
+        _optionalTextsGO.Add(_mainTextMeshProUGUI);
 
     }
 
@@ -43,20 +32,16 @@ public class OnSelectEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         Debug.Log("Pointer Entered");
 
 
-        for (int i = 0; i < _optionalTextsGO.Count; i++)
+        foreach (TextMeshProUGUI v in _optionalTextsGO)
         {
-            _optionalTextsGO[i].fontSharedMaterial = _targetMaterial;
-            _optionalTextsGO[i].fontWeight = FontWeight.SemiBold;
+            v.fontSharedMaterial = _targetMaterial;
+            v.fontWeight = FontWeight.SemiBold;
 
         }
-
-        if (_mainTextMeshProUGUI != null)
+        if (_addArrow)
         {
-            _mainTextMeshProUGUI.fontSharedMaterial = _targetMaterial;
-            _mainTextMeshProUGUI.fontWeight = FontWeight.SemiBold;
             _mainTextMeshProUGUI.text = ">" + _mainOriginalText;
         }
-
 
         _buttonBackground.color = new Color(1, 1, 1, 1);
     }
@@ -66,21 +51,14 @@ public class OnSelectEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         Debug.Log("Pointer Exited");
 
 
-        for (int i = 0; i < _optionalTextsGO.Count; i++)
+        foreach (TextMeshProUGUI v in _optionalTextsGO)
         {
-            _optionalTextsGO[i].fontSharedMaterial = _originMaterial;
-            _optionalTextsGO[i].fontWeight = FontWeight.Regular;
+            v.fontSharedMaterial = _originMaterial;
+            v.fontWeight = FontWeight.Regular;
 
         }
 
-        if (_mainTextMeshProUGUI != null)
-        {
-            _mainTextMeshProUGUI.fontSharedMaterial = _originMaterial;
-            _mainTextMeshProUGUI.fontWeight = FontWeight.Regular;
-            _mainTextMeshProUGUI.text = _mainOriginalText;
-        }
-
-
+        _mainTextMeshProUGUI.text = _mainOriginalText;
         _buttonBackground.color = new Color(1, 1, 1, 0);
 
     }

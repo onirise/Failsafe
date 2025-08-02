d72d523255cf8a01e0897dba10c8643072da2d81
dubashitbl <alexwest12567@gmail.com>
2025-08-02 03:53:29 +0200

    added examination

--- Assets/Failsafe/Scripts/Timer.cs
+++ Assets/Failsafe/Scripts/Timer.cs
@@ -3,11 +3,22 @@ using Cysharp.Threading.Tasks;
 
 public static class Timer
 {
-    public static async UniTask StartTimer(float totalTime, float tickInterval = 1, Action onTick = null, Action onComplete = null)
+    public static async UniTask StartTimer(float totalTime, float tickInterval = 1, Action onTick = null, Action onComplete = null, bool allowFinalAction = false)
     {
         float elapsed = 0f;
         while (elapsed < totalTime)
         {
+            if (elapsed + tickInterval > totalTime)
+            {
+                float remain = totalTime - elapsed;
+                await UniTask.Delay(TimeSpan.FromSeconds(remain));
+                
+                if (allowFinalAction)
+                    onTick?.Invoke();
+                
+                break;
+            }
+            
             await UniTask.Delay(TimeSpan.FromSeconds(tickInterval));
             elapsed += tickInterval;
 
